b5f3c71dc0fd98d89eba3ed8a0843aea243c22de
yomoyomo9411-wq <yomoyomo9411@gmail.com>
2026-03-04 02:13:41 +0900

    Add Leaflet map and update dependencies

--- app/Map.tsx
+++ app/Map.tsx
@@ -3,30 +3,34 @@
 import dynamic from "next/dynamic";
 import "leaflet/dist/leaflet.css";
 
-const MapContainer = dynamic(
+import { LatLngExpression } from "leaflet";
+
+const MapContainer: any = dynamic(
   () => import("react-leaflet").then((mod) => mod.MapContainer),
   { ssr: false }
 );
 
-const TileLayer = dynamic(
+const TileLayer: any = dynamic(
   () => import("react-leaflet").then((mod) => mod.TileLayer),
   { ssr: false }
 );
 
-const Marker = dynamic(
+const Marker: any = dynamic(
   () => import("react-leaflet").then((mod) => mod.Marker),
   { ssr: false }
 );
 
-const Popup = dynamic(
+const Popup: any = dynamic(
   () => import("react-leaflet").then((mod) => mod.Popup),
   { ssr: false }
 );
 
 export default function Map() {
+  const position: LatLngExpression = [35.681236, 139.767125];
+
   return (
     <MapContainer
-      center={[35.681236, 139.767125]}
+      center={position}
       zoom={13}
       style={{ height: "500px", width: "100%" }}
     >
@@ -34,7 +38,7 @@ export default function Map() {
         attribution="&copy; OpenStreetMap contributors"
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[35.681236, 139.767125]}>
+      <Marker position={position}>
         <Popup>東京駅</Popup>
       </Marker>
     </MapContainer>
